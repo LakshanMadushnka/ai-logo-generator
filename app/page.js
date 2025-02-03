cc8c655b813ac3124c85fbfe619ba9adbec9214d
LakshanMadushnka <lranasinghe286@gmail.com>
2025-02-03 21:48:34 +0530

    Add Hero component and update layout in the home page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
-      <h2>Hello World</h2>
-      <Button>Suscribe</Button>
+      <Hero />
     </div>
   );
 }
